40b8506bd29fb2b6dbcbb029a0fadc559f469d86
Renato <renato@ejcm.com.br>
2023-03-22 02:13:04 -0300

    feature: controllers e routas de usuário criadas e testadas

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -5,6 +5,10 @@ const userController = require("../controllers/userController");
 const router = require("express")();
 
 //rotas usuário
-router.post("/user", userController.create)
+router.post("/user", userController.create);
+router.get("/user", userController.index);
+router.get("/user/:id", userController.show);
+router.put("/user/:id", userController.update);
+router.delete("/user/:id", userController.destroy);
 
 module.exports = router;
